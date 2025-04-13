9ffc444ee95631d06a861d5ef5821b8e0bb7b919
Wiktor <w2002wiktor@o2.pl>
2025-04-13 21:25:13 +0200

    Poprawa settings.py - naprawa błędów składniowych

--- products/views.py
+++ products/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
-<<<<<<< HEAD
 from .models import Product  # Załóżmy, że masz model Product
 
 def home(request):
     products = Product.objects.all()  # Pobierz wszystkie produkty
     return render(request, 'products/home.html', {'products': products})
-=======
-
-# Create your views here.
->>>>>>> b771156e4ff712a61c31eaea92eaea540009bd72
